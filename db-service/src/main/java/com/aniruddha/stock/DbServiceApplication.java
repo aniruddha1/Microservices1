b1f24fb84692e947c48f43f0f1290f65b91f1be4
Ani G <aniruddha.godawale@gmail.com>
2018-03-04 10:32:16 +0000

    New project

---
+++ db-service/src/main/java/com/aniruddha/stock/DbServiceApplication.java
@@ -0,0 +1,16 @@
+package com.aniruddha.stock;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = "com.aniruddha.stock.repository")
+
+@SpringBootApplication
+public class DbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbServiceApplication.class, args);
+	}
+}
