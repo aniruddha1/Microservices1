fe08d26cbe6283cd167c513c34a3d62b8637558d
Ani G <aniruddha.godawale@gmail.com>
2018-03-04 21:58:39 +0000

    update 1

---
+++ stock-service/src/main/java/com/aniruddha/stock/config/Config.java
@@ -0,0 +1,19 @@
+package com.aniruddha.stock.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class Config {
+	
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate() {
+		
+		return new RestTemplate();
+		
+	}
+
+}
